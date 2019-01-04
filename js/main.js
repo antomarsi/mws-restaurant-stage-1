83f485fa69a6ac06459b2f203673db27f4f313e8
Antônio Marco da Silva <antonio.silva@bludata.com.br>
2019-01-04 16:21:02 -0200

    Changed css

--- js/main.js
+++ js/main.js
@@ -76,9 +76,9 @@ initMap = () => {
         center: [40.722216, -73.987501],
         zoom: 12,
         scrollWheelZoom: false
-      });
+  });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYW50b21hcnNpIiwiYSI6ImNqcWljZjY0cDB1eDg0NGwyMjA3dnN5M3gifQ.mqMvHzk7soAfXuQG-YglqQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
